54b9a663dd85feb0a30fcf38f39f89a7b3383ad5
Alexandr <blix@list.ru>
2025-01-13 13:48:05 +0300

    Права доступа база

--- users/views.py
+++ users/views.py
@@ -1,9 +1,41 @@
-from rest_framework.generics import UpdateAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.decorators import permission_classes
+from rest_framework.generics import (
+    UpdateAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+)
 from rest_framework import filters
+from rest_framework.permissions import AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 
 from users.models import User, Payment
 from users.serializers import UserSerializer, PaymentSerializer
+
+
+class UserCreateApiView(CreateAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def perform_create(self, serializer):
+        user = User.objects.all()
+        user.set_password(user.password)
+        user.save()
+
+
+class UserDeleteApiView(DestroyAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserListApiView(ListAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserUpdateApiView(UpdateAPIView):

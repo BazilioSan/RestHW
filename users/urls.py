22c2986b3109f2505c2e96d89ea623cdf9155417
Alexandr <blix@list.ru>
2025-01-13 23:43:28 +0300

    Форматтер ок

--- users/urls.py
+++ users/urls.py
@@ -16,8 +16,16 @@ from django.urls import path
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(permission_classes = (AllowAny,)), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(permission_classes = (AllowAny,)), name="token_refresh"),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
     path("<int:pk>/update/", UserUpdateApiView.as_view(), name="user_update"),
     path("retrieve/<int:pk>/", UserRetrieveApiView.as_view(), name="user_retrieve"),
     path("payment/", PaymentListApiView.as_view(), name="payment"),
